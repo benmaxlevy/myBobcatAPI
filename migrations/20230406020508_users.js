09381c791281fb3c0133e827e5a5fab06e3ef03b
Ben Levy <general@simplexwebsites.com>
2023-05-27 15:01:14 -0400

    Make events migration (including foreign key)

--- migrations/20230406020508_users.js
+++ migrations/20230406020508_users.js
@@ -4,7 +4,7 @@
  */
 exports.up = function(knex) {
  return knex.schema.createTable("users", (table) => {
-  table.increments();
+  table.increments("id").primary().unsigned();
   table.string("name").notNullable();
   table.string('email').notNullable().unique();
   table.string('password').notNullable();
