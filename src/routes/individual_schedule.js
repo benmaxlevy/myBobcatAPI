a6803b808435c7099c93f0d7c4748c9674f4f6bb
Ben Levy <general@simplexwebsites.com>
2023-12-29 17:38:32 -0500

    Finish individual schedules (create put and delete)

--- src/routes/individual_schedule.js
+++ src/routes/individual_schedule.js
@@ -1,17 +1,17 @@
 const express = require("express"),
-    indiviudalScheduleRouter = express(),
+    individualScheduleRouter = express(),
     bodyParser = require("body-parser"),
     knex = require("../knexClient"),
     isLoggedIn = require("../middleware/isLoggedIn");
 
 // set up bodyParser
-indiviudalScheduleRouter.use(bodyParser.urlencoded({ extended: false }));
+individualScheduleRouter.use(bodyParser.urlencoded({ extended: false }));
 
 // get a user's class given the period and day number
-indiviudalScheduleRouter.get("/day/:day/period/:period", isLoggedIn, async (req, res, next) => {
+individualScheduleRouter.get("/day/:day/period/:period", isLoggedIn, async (req, res, next) => {
     try {
         const userId = req.jwt.id;
-
+        console.log(req.params)
         // get individual_schedule where day_number=day & period=period
         let className = await knex("individual_schedules")
             .where({
@@ -19,7 +19,7 @@ indiviudalScheduleRouter.get("/day/:day/period/:period", isLoggedIn, async (req,
                 user_id: userId,
                 period: req.params.period
             });
-        className = className.class_name;
+        className = className[0].class_name;
 
         return res.status(200).json({className: className});
 
@@ -28,15 +28,15 @@ indiviudalScheduleRouter.get("/day/:day/period/:period", isLoggedIn, async (req,
     }
 });
 
-// post
-indiviudalScheduleRouter.post("/", isLoggedIn, async (req, res, next) => {
+// post (one day/period combination)
+individualScheduleRouter.post("/", isLoggedIn, async (req, res, next) => {
     try {
         const userId = req.jwt.id;
 
         await knex("individual_schedules")
             .insert({
                 period: req.body.period,
-                day_name: req.body.day_name,
+                day_number: req.body.day_number,
                 class_name: req.body.class_name,
                 user_id: userId
             });
@@ -48,4 +48,49 @@ indiviudalScheduleRouter.post("/", isLoggedIn, async (req, res, next) => {
     }
 });
 
-module.exports = indiviudalScheduleRouter;
+// put (one day/period combination)
+individualScheduleRouter.put("/day/:day/period/:period", isLoggedIn, async (req, res, next) => {
+    try {
+        const userId = req.jwt.id;
+
+        await knex("individual_schedules")
+            .where({
+                period: req.params.period,
+                day_number: req.params.day,
+                user_id: userId
+            })
+            .update({
+                period: req.body.period,
+                day_number: req.body.day_number,
+                class_name: req.body.class_name
+            });
+
+        return res.status(200).json({message: "Success!"});
+
+    } catch (e) {
+        next(e);
+    }
+});
+
+// delete
+
+individualScheduleRouter.delete("/day/:day/period/:period", isLoggedIn, async (req, res, next) => {
+    try {
+        const userId = req.jwt.id;
+
+        await knex("individual_schedules")
+            .where({
+                period: req.params.period,
+                day_number: req.params.day,
+                user_id: userId
+            })
+            .del();
+
+        return res.status(200).json({message: "Success!"});
+
+    } catch (e) {
+        next(e);
+    }
+});
+
+module.exports = individualScheduleRouter;
